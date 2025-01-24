f1d0c53ee74aafd4f90294e9299218a489c089c5
Kyle Thomson <kiiadi@gmail.com>
2025-01-24 08:34:43 -0800

    Massive commit - oops - have done bunch of stuff
    - started on getting auth going - currently uses API key
    - can load, login, add data points
    - refactored to single dynamodb table
    - integrated login with google auth on server
    - started, but only partially working, google login

--- src/main/kotlin/com/kiiadi/gradle/cdk/CdkPlugin.kt
+++ src/main/kotlin/com/kiiadi/gradle/cdk/CdkPlugin.kt
@@ -1,13 +1,11 @@
 package com.kiiadi.gradle.cdk
 
-import com.github.gradle.node.NodeExtension
 import com.github.gradle.node.NodePlugin
 import com.github.gradle.node.npm.task.NpxTask
 import com.github.jengelman.gradle.plugins.shadow.ShadowPlugin
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.initialization.resolve.RepositoriesMode
 import org.gradle.api.plugins.ApplicationPlugin
 import org.gradle.api.plugins.JavaApplication
 import org.gradle.api.tasks.Delete
@@ -20,6 +18,8 @@ class CdkPlugin: Plugin<Project> {
 
         project.extensions.add(CdkExtension.NAME, CdkExtension::class.java)
         val cdkExtension = project.extensions.getByType<CdkExtension>()
+
+        val cdkCli = cdkExtension.cdkVersion.map { "aws-cdk@$it" }
         project.plugins.apply(NodePlugin::class.java)
 
         project.plugins.apply(ApplicationPlugin::class.java)
@@ -30,18 +30,31 @@ class CdkPlugin: Plugin<Project> {
         project.plugins.apply(ShadowPlugin::class.java)
 
         val cdkSynth = project.tasks.register<NpxTask>("cdkSynth") {
-            val shadowJarTask = project.tasks.getByName("shadowJar") as ShadowJar
             group = TASK_GROUP
-            command.set(project.extensions.getByType<CdkExtension>().cdkVersion.map { "aws-cdk@$it" })
 
+            val shadowJarTask = project.tasks.getByName("shadowJar") as ShadowJar
             val shadowJar = shadowJarTask.outputs.files.first()
+            inputs.files(shadowJar)
+            outputs.dir("cdk.out")
+
+            command.set(cdkCli)
+
             args.set(listOf("synth", "--app", "\"java -jar ${shadowJar}\"", "-q"))
             dependsOn(shadowJarTask)
         }
         project.tasks.getByName("build").dependsOn(cdkSynth)
 
-        val cleanCdk = project.tasks.register<Delete>("cleanCdk") {
+        project.tasks.register<NpxTask>("cdkDeploy") {
+            group = TASK_GROUP
 
+            command.set(cdkCli)
+
+            args.set(listOf("deploy", "--app", "cdk.out", "--all"))
+
+            dependsOn(cdkSynth)
+        }
+
+        val cleanCdk = project.tasks.register<Delete>("cleanCdk") {
             delete("cdk.out")
         }
         project.tasks.getByName("clean").dependsOn(cleanCdk)
