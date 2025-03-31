661145f3d14f133356e2632e797d383d8d460557
Kyle Thomson <kiiadi@gmail.com>
2025-03-31 16:05:29 -0700

    Setting up release bits (#6)
    * Setting up build, release and publish
    
    * moving version to properties
    
    * Adding dependency for release
    
    * Updating website

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,21 +3,28 @@ plugins {
     kotlin("jvm") version "2.0.21"
     id("com.gradle.plugin-publish") version "1.3.0"
     `kotlin-dsl`
+    id("net.researchgate.release") version "3.1.0"
 }
 
 group = "com.kiiadi"
-version = "0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
     gradlePluginPortal()
 }
 
+tasks.afterReleaseBuild.map { it.dependsOn(tasks.publishPlugins) }
+
 gradlePlugin {
+    vcsUrl = "https://github.com/kiiadi/gradle-cdk-plugin"
+    website = "https://github.com/kiiadi/gradle-cdk-plugin/blob/main/README.md"
     plugins {
         create("gradle-cdk-plugin") {
             id = "com.kiiadi.gradle-cdk-plugin"
+            displayName = "Gradle CDK Plugin"
+            description = "This plugin allows you to create Java-based AWS CDK projects without having to have a local node version installed, or to install the CDK CLI separately."
             implementationClass = "com.kiiadi.gradle.cdk.CdkPlugin"
+            tags = listOf("aws", "cdk", "iac", "java")
         }
     }
 }
